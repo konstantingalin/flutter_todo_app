4cd37c2cea8a04377043a9b495b296b5798c1aa3
Konstantin <38990885+konstantingalin@users.noreply.github.com>
2023-03-02 19:41:32 +0600

    init

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo App'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: const Text('This is home screen'),
+      ),
+    );
+  }
+}
